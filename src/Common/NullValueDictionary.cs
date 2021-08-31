badc4020b51ca7368b7fdbaa9b23fae32154e476
Mathieu De Raedt <mathieu@deraedt.dev>
2021-08-31 15:44:52 +0200

    Unable ctors

--- src/Common/NullValueDictionary.cs
+++ src/Common/NullValueDictionary.cs
@@ -3,6 +3,15 @@ using System.Runtime.Serialization;
 
 namespace Common {
     public class NullValueDictionary<TKey, TValue> : Dictionary<TKey, TValue> {
+        public NullValueDictionary() { }
+        public NullValueDictionary(IDictionary<TKey, TValue> dictionary) : base(dictionary) { }
+        public NullValueDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey>? comparer) : base(dictionary, comparer) { }
+        public NullValueDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection) : base(collection) { }
+        public NullValueDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection, IEqualityComparer<TKey>? comparer) : base(collection, comparer) { }
+        public NullValueDictionary(IEqualityComparer<TKey>? comparer) : base(comparer) { }
+        public NullValueDictionary(int capacity) : base(capacity) { }
+        public NullValueDictionary(int capacity, IEqualityComparer<TKey>? comparer) : base(capacity, comparer) { }
+        protected NullValueDictionary(SerializationInfo info, StreamingContext context) : base(info, context) { }
         public new TValue this[TKey key] => TryGetValue(key, out var value) ? value : default;
     }
 }
