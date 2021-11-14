a38a9cf51f9cc057841295fb6e84fc28992bd50d
Mathieu De Raedt <mathieu@deraedt.dev>
2021-11-15 00:45:36 +0100

    Update fluent assertions to new version

--- src/Common/Extensions/FluentResultExtensions.cs
+++ src/Common/Extensions/FluentResultExtensions.cs
@@ -6,10 +6,10 @@ using FluentResults;
 namespace Common.Extensions {
     public static class FluentResultExtensions {
         private static readonly string DefaultSeparator = Environment.NewLine;
-        
+
         /// <summary>
-        /// Combines all the reasons of the result in one single string
-        /// The default separator is <see cref="Environment.NewLine"/>
+        ///     Combines all the reasons of the result in one single string
+        ///     The default separator is <see cref="Environment.NewLine" />
         /// </summary>
         /// <param name="result">Result with reasons</param>
         /// <typeparam name="T"></typeparam>
@@ -17,19 +17,19 @@ namespace Common.Extensions {
         public static string CombineMessage<T>(this Result<T> result) {
             return Combine(result.Successes, DefaultSeparator);
         }
-        
+
         /// <summary>
-        /// Combines all the reasons of the result in one single string
-        /// The default separator is <see cref="Environment.NewLine"/>
+        ///     Combines all the reasons of the result in one single string
+        ///     The default separator is <see cref="Environment.NewLine" />
         /// </summary>
         /// <param name="result">Result with reasons</param>
         /// <returns>One string with all the reasons, concatenated with the default separator</returns>
         public static string CombineMessage(this Result result) {
             return Combine(result.Reasons, DefaultSeparator);
         }
-        
+
         /// <summary>
-        /// Combines all the reasons of the result in one single string
+        ///     Combines all the reasons of the result in one single string
         /// </summary>
         /// <param name="result">Result with reasons</param>
         /// <param name="separator">Separator to concatenate all the results</param>
@@ -38,9 +38,9 @@ namespace Common.Extensions {
         public static string CombineMessage<T>(this Result<T> result, string separator) {
             return Combine(result.Reasons, separator);
         }
-        
+
         /// <summary>
-        /// Combines all the reasons of the result in one single string
+        ///     Combines all the reasons of the result in one single string
         /// </summary>
         /// <param name="result">Result with reasons</param>
         /// <param name="separator">Separator to concatenate all the results</param>
@@ -49,13 +49,12 @@ namespace Common.Extensions {
             return Combine(result.Reasons, separator);
         }
 
-        private static string Combine(List<Reason> reasons, string separator) {
+        private static string Combine(List<IReason> reasons, string separator) {
             return reasons.Select(r => r.Message).Aggregate((a, b) => a + separator + b);
         }
-        
-        private static string Combine(List<Success> successes, string separator) {
+
+        private static string Combine(List<ISuccess> successes, string separator) {
             return successes.Select(r => r.Message).Aggregate((a, b) => a + separator + b);
         }
     }
-
 }
