2ac8c8f07eebc567dcb048e038b68dea07539957
Mathieu De Raedt <mathieu@deraedt.dev>
2022-04-20 19:25:38 +0200

    Add random enum value

---
+++ src/Common/Extensions/RandomExtensions.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Common.Extensions;
+
+public static class RandomExtensions {
+	public static T NextEnum<T>(this Random random) where T: System.Enum{
+		var values = Enum.GetValues(typeof(T));
+		return (T)values.GetValue(random.Next(values.Length));
+	}
+}
---
+++ tests/Common.tests/Extensions/RandomExtensionTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using Common.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Common.tests.Extensions; 
+
+public class RandomExtensionTests {
+	public enum MyEnum {
+		A,
+		B,
+		C,
+		D,
+		E,
+		F,
+		G,
+		H
+	}
+	
+	[Fact]
+	public void NextEnum_ShouldBeAValue_WhenGivenEnum() {
+		var random = new Random();
+		var result = random.NextEnum<MyEnum>();
+		result.Should().BeOneOf(MyEnum.A, MyEnum.B, MyEnum.C, MyEnum.D, MyEnum.E, MyEnum.F, MyEnum.G, MyEnum.H);
+	}
+	
+	[Fact]
+	public void NextEnum_ShouldHaveAllValues_WhenCallingItMultipleTimes() {
+		var random = new Random();
+		
+		var results = new MyEnum[1000];
+		for (var i = 0; i < results.Length; i++) {
+			results[i] = random.NextEnum<MyEnum>();
+		}
+		
+		var distinctResults = results.Distinct().ToList();
+		distinctResults.Count.Should().Be(8);
+	}
+}
